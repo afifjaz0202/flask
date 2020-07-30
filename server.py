1f578c8927325a3918e9f8b61e1232cddce25770
Afif Jazimin <afifjaz0202@gmail.com>
2020-07-30 15:56:29 +0800

    second commit

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def index():
     return '<img src="https://preview.redd.it/gap5ah93ciy11.jpg?auto=webp&s=a4497d2ac778a9d46c0f254bb55ae4b502a60b99" />'
 
+@app.route("/another")
+def show():
+    return '<h1>Penguin Asscheeks</h1>'
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
     app.run()
