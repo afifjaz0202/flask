e4b6b87da3b6020f268417a4cd7f6809b8cfa2f6
Afif Jazimin <afifjaz0202@gmail.com>
2020-07-30 15:43:32 +0800

    first commit

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/") # Revisit decorators if you unclear of this syntax
+def index():
+    return '<img src="https://preview.redd.it/gap5ah93ciy11.jpg?auto=webp&s=a4497d2ac778a9d46c0f254bb55ae4b502a60b99" />'
+
+
+if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
+    app.run()
