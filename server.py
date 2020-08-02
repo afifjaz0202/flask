206f218ffa4b1325b4e16345d1a587e1364971ad
Afif Jazimin <afifjaz0202@gmail.com>
2020-08-02 21:12:30 +0800

    added sign in and sign out button

--- server.py
+++ server.py
@@ -14,5 +14,10 @@ def user(name):
     name = name.upper()
     return render_template('index.html', name=name)
 
+@app.route("/login")
+def login():
+    signed_in = True # we are hardcoding this just to demonstrate how we can do conditionals in our template files, in future we won't be hardcoding this.
+    return render_template('index.html', signed_in=signed_in)
+
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
     app.run(debug = True)
