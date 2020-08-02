bcf77e38e6b7e1ae91ba4286d191123ededd408a
Afif Jazimin <afifjaz0202@gmail.com>
2020-08-02 21:02:26 +0800

    showed name from url parameter in template

--- server.py
+++ server.py
@@ -9,9 +9,10 @@ def index():
 def show():
     return '<h1>Penguin Asscheeks</h1>'
 
-@app.route("/user/<username>")
-def user(username):
-    return (f"Hi {username[:]}, you Bitchass Mofo")
+@app.route("/<name>")
+def user(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
     app.run(debug = True)
